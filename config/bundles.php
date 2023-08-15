f56d0deb36cd31d2ee6610476e0e69ac5d434d7a
Adrián García <akrian231@gmail.com>
2023-08-15 13:52:02 +0200

    Revert "build: install doctrine"
    This reverts commit b9080f367325a4b7ca41cf57ad85039fd7ed91d0.

--- config/bundles.php
+++ config/bundles.php
@@ -5,6 +5,4 @@ return [
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
