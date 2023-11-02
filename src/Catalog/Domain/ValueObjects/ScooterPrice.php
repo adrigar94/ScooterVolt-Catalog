0f935a1dbe149f8cfb95b690d2242fdd3b173132
adrigar94 <akrian231@gmail.com>
2023-11-02 15:26:24 +0100

    refactor: changes from new version of adrigar94/value-object-craft

--- src/Catalog/Domain/ValueObjects/ScooterPrice.php
+++ src/Catalog/Domain/ValueObjects/ScooterPrice.php
@@ -4,9 +4,38 @@ declare(strict_types=1);
 
 namespace ScooterVolt\CatalogService\Catalog\Domain\ValueObjects;
 
+use Adrigar94\ValueObjectCraft\Domain\Currency\CurrencyValueObject;
 use Adrigar94\ValueObjectCraft\Domain\Price\PriceValueObject;
 
 class ScooterPrice extends PriceValueObject
 {
+    private $priceConversions = [];
 
+    public static function createPrice(float $price, CurrencyValueObject $currency, array $priceConversions = []): self
+    {
+        $price = parent::createPrice($price, $currency);
+        $price->priceConversions = $priceConversions;
+        return $price;
+    }
+
+    public static function fromNative($native)
+    {
+        $price = parent::fromNative($native);
+
+        $price->priceConversions = $native['price_conversions'] ?? [];
+
+        return $price;
+    }
+
+    public function toNative(): array
+    {
+        $native = parent::toNative();
+        $native['price_conversions'] = $this->priceConversions;
+        return $native;
+    }
+
+    public function getPriceConversions(): array
+    {
+        return $this->priceConversions;
+    }
 }
